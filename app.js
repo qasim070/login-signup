95ca2cae624b4304569216151a752b387cccf4b5
qasim070 <mujahidsoomro5@gmail.com>
2023-09-14 20:27:59 +0500

    update

--- app.js
+++ app.js
@@ -1,44 +1,58 @@
 
 let latestUsers = []
-function signup(){
-    // e.preventDefault();
+function signup(event){
+    event.preventDefault();
+
     
     let regUsername = document.getElementById("myUsername").value;
     let regEmail = document.getElementById("myEmail").value;
     let regPass = document.getElementById("myPassword").value;
-    // let usernameError = document.getElementById("errorUsername");
     let emailError = document.getElementById("errorEmail");
     let passError = document.getElementById("errorPass");
+    let fetchData = JSON.parse(localStorage.getItem('usersList'))
+    let checkPrevData = fetchData.find( e => e.email == regEmail.value)
+    alert(checkPrevData)
 
-    if(regEmail.includes("@")){
-        emailError.innerHTML = "";
-    }
+    if(regUsername == ""  || regEmail == "" || regPass == "" ){
+        swal({
+            icon: "warning",
+            title: "Don't Leave anything Empty",
+            });
+    }else{
 
-    if(!regEmail.includes("@")){
-        emailError.innerHTML = "Please Enter a Valid Email Address";
-    }
-    if(regPass.length < 8){
-        passError.innerHTML = "Password length must be greater than 8 characters";
-    }
-    else{
-        latestUsers.push({
-            email : regEmail,
-            password : regPass,
-        })
-            localStorage.setItem('usersList', JSON.stringify(latestUsers))
-            regEmail = "";
-            regPass = "";
-            regUsername = "";
-            swal({
-                icon: "success",
-                title: "Account Created Successfuly",
-                showCancelButton: true,
-                showConfirmButton: true,
-                confirmButtonText: "Redirect to Dashboard",   
-                closeOnConfirm: true 
-                }, function() {
-                    window.location = '/';
-                });
+        if(regEmail.includes("@")){
+            emailError.innerHTML = "";
+        }
+        if(!regEmail.includes("@")){
+            emailError.innerHTML = "Please Enter a Valid Email Address";
+        }
+        if(regPass.length < 8){
+            passError.innerHTML = "Password length must be greater than 8 characters";
+        }
+        // else{
+            if(checkPrevData){
+                alert("ij");
+            }else{
+                latestUsers.push({
+                    username: regUsername,
+                    email : regEmail,
+                    password : regPass,
+                })
+                    if(localStorage.setItem('usersList', JSON.stringify(latestUsers))){
+                        swal({
+                            icon: "success",
+                            title: "Account Created Successfuly",
+                            showCancelButton: true,
+                            showConfirmButton: true,
+                            confirmButtonText: "Redirecting you to Dashboard",   
+                            closeOnConfirm: true 
+                            }, function() {
+                                window.location = '/';
+                            });
+                    }
+                }
+         
+                // }
         }
     }
 
@@ -46,22 +60,22 @@ function login(){
     let logEmail = document.getElementById("logEmail");
     let logPass = document.getElementById("logPassword");
     let fetchData = JSON.parse(localStorage.getItem('usersList'))
-    let checkUser = fetchData.find( e => e.email == logEmail.value && e.password == logPass.value)
-    if(logEmail.value === undefined && logPass.value === undefined){
+    if(logEmail.value == "" || logPass == ""  ){
         swal({
             icon: "warning",
-            title: "Incorrect Credentials",
-            text: "Please Check the email pr Password",
-          });
-
+            title: "Inputs can not empty",
+            text: "Email or Password Field is empty",
+        });
+    }else{
+        let checkUser = fetchData.find( e => e.email == logEmail.value && e.password == logPass.value)
         if(!checkUser){
             swal({
                 icon: "error",
                 title: "Incorrect Credentials",
-                text: "Please Check the email pr Password",
+                text: "Please Check the email or Password",
               });
         }else{
-            window.document.location = "./"
+            window.document.location = "./";
         }
     }
     // else if( check.password != logPass.value){
